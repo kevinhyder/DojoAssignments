1bbffac16380e0fc48b26a91955f815dd7ed3170
kevinhyder <kevin.hyder@me.com>
2017-11-14 15:03:46 -0800

    I Do Declare Exercise

---
+++ WebFundamentals/JavaScript/i-do-declare.js
@@ -0,0 +1,23 @@
+var million = 1000000
+console.log("One Million:", million)
+var pi = 3.14
+console.log("Pi:", pi)
+var goldenRatio = 1.16
+console.log("Golden Ratio:", goldenRatio)
+var negSeventeen = -17
+console.log("Negative Seventeen", negSeventeen)
+
+var fullName = "Kevin Michael Hyder"
+console.log("My Full Name:", fullName)
+var birthDate = "December 19, 1978"
+console.log("My Birthdate:", birthDate)
+var favQuote = "And those who were seen dancing were thought to be insane by those who could not hear the music."
+console.log("My Favorite Quote:", favQuote)
+
+var wasChef = true
+console.log("I was a Chef:", wasChef)
+var wasCop = false
+console.log("I was a Cop:", wasCop)
+
+var undefinedVariable
+console.log("This variable is:", undefinedVariable)
