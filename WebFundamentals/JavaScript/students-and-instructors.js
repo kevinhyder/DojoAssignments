416d627bcf5d32cff3aca300551ffec4c9a1b6a0
kevinhyder <kevin.hyder@me.com>
2017-11-15 14:20:30 -0800

    Students and Instructors Exercise

---
+++ WebFundamentals/JavaScript/students-and-instructors.js
@@ -0,0 +1,40 @@
+var students = [ 
+    {first_name:  'Michael', last_name : 'Jordan'},
+    {first_name : 'John', last_name : 'Rosales'},
+    {first_name : 'Mark', last_name : 'Guillen'},
+    {first_name : 'KB', last_name : 'Tonel'}
+]
+
+function studentsAndInstructors(arr) {
+
+    for( var i = 0; i < arr.length; i++ ) {
+        console.log(arr[i].first_name, arr[i].last_name)
+    }
+}   
+studentsAndInstructors(students)
+
+
+var users = {
+    'Students': [ 
+        {first_name:  'Michael', last_name : 'Jordan'},
+        {first_name : 'John', last_name : 'Rosales'},
+        {first_name : 'Mark', last_name : 'Guillen'},
+        {first_name : 'KB', last_name : 'Tonel'}
+     ],
+    'Instructors': [
+        {first_name : 'Michael', last_name : 'Choi'},
+        {first_name : 'Martin', last_name : 'Puryear'}
+     ]
+    }
+   
+function studentsAndInstructorsBonus(obj) {
+    console.log("Students")
+    for( var i = 0; i < users.Students.length; i++ ) {
+        console.log( (i + 1).toString(), "-", users.Students[i].first_name, users.Students[i].last_name, "-", (users.Students[i].first_name.length + users.Students[i].last_name.length) )
+    }
+    console.log("Instructors")
+    for(var i = 0; i < users.Instructors.length; i++ ) {
+        console.log( (i + 1).toString(), "-", users.Instructors[i].first_name, users.Instructors[i].last_name, "-", (users.Instructors[i].first_name.length + users.Instructors[i].last_name.length) )
+    }
+}
+studentsAndInstructorsBonus(users)
