968d10e7474e31a0122a45997d663ad54c69ebb5
kevinhyder <kevin.hyder@me.com>
2017-11-15 04:21:56 -0800

    Added additional functionality in case of missing arguments

---
+++ WebFundamentals/JavaScript/range-print.js
@@ -0,0 +1,13 @@
+function rangePrint(start, end, skip) {
+    if(skip == undefined) {
+        skip = 1
+    }
+    if(end == undefined) {
+        end = start
+        start = 0
+    }
+    for(var i = start; i < end; i += skip) {
+        console.log(i)
+    }
+}
+rangePrint(10)
