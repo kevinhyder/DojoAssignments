8d09040c023812489a6958aa51f5a68944c16c0d
kevinhyder <kevin.hyder@me.com>
2017-11-21 15:41:10 -0800

    Improved Functionality

--- WebFundamentals/jQuery/ninja-to-cat/jquery.js
+++ WebFundamentals/jQuery/ninja-to-cat/jquery.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+    
     $('img').click(function() { 
-        $(this).attr('src', $(this).attr("data-alt-src"));
-        
+        var currSrc = $(this).attr('src');
+        var altSrc = $(this).attr('data-alt-src')
+        $(this).attr('data-alt-src', currSrc);
+        $(this).attr('src', altSrc);
     });
 });
