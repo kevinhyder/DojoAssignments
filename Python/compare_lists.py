6a7075813b9ab855afdfecb9f0d0293602d75741
kevinhyder <kevin.hyder@me.com>
2017-12-12 01:25:01 -0800

    Compare Lists Exercise

---
+++ Python/compare_lists.py
@@ -0,0 +1,11 @@
+def compare_lists(list_one, list_two):
+    # 
+    if list_one == list_two:
+        print "The lists are the same."
+    else:
+        print "The lists are not the same."
+
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+compare_lists(list_one, list_two)
