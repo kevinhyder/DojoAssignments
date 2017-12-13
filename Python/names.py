4d0925294c4a8702cd81cde72d56d7e5501f4c31
kevinhyder <kevin.hyder@me.com>
2017-12-13 13:11:20 -0800

    Names Exercise

---
+++ Python/names.py
@@ -0,0 +1,30 @@
+def names(list):
+    for element in list:
+        print element['first_name'], element['last_name']
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+names(students)
+
+def names_part_2(dictionary):
+    count = 1
+    for element in dictionary[list]:
+        print count, '-', element['first_name'], element['last_name'], '-', len(element)
+        count += 1
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+names_part_2(users)
