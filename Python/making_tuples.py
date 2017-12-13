f4d7eabfa8d2217baa92bf0815fa8c827c25d182
kevinhyder <kevin.hyder@me.com>
2017-12-13 13:37:30 -0800

    Making Tuples Exercise

---
+++ Python/making_tuples.py
@@ -0,0 +1,11 @@
+def making_tuples(dictionary):
+        print dictionary.items()
+
+
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+making_tuples(my_dict)
